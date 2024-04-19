b8878a7f30f6c6ce2db535068e695faf16fb1e24
Harsh Kapil <kapilharsh1999@gmail.com>
2024-04-19 23:59:31 +0530

    jobs page

--- src/app/(dashboard)/dashboard/(routes)/jobs/page.tsx
+++ src/app/(dashboard)/dashboard/(routes)/jobs/page.tsx
@@ -2,11 +2,14 @@
 
 import React, { useEffect } from 'react'
 import axios from 'axios'
+import { Button } from '@/components/ui/button'
+
+import { getCookie } from 'cookies-next';
 
 const Job = () => {
-
-  useEffect(() => {
-    axios.get(`http://localhost:8000/api/v1/recruitersearch`)
+  const myCookie = getCookie('mindprism-token')
+  /* useEffect(() => {
+    axios.get(`http://localhost:8000/api/v1/recruitersearch`, { headers: { 'Authorization': `Bearer ${myCookie}` } })
           .then((response) => {
             console.log(response.data);
           })         
@@ -14,10 +17,21 @@ const Job = () => {
             console.log(error);
           })
   })
+  console.log(myCookie) */
 
+  const getUser = () => {
+    axios.get(`http://localhost:8000/api/v1/users/profile`, { headers: { 'Authorization': `Bearer ${myCookie}` } })
+          .then((response) => {
+            console.log(response.data);
+          })         
+          .catch((error) => {
+            console.log(error);
+          })
+  }
   return (
     <div className=' flex flex-col'>
       <h1 className=' text-2xl Font-bold'>Jobs</h1>
+      <Button onClick={() => getUser()}>Get User</Button>
     </div>
   )
 }
